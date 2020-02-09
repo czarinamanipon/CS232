2a5df0cc7cca27a65aee63ec4fa459dd6f632cda
Czarina Manipon <cm80117n@pace.edu>
2020-02-08 20:22:40 -0500

    completed task 3

---
+++ lab2/task3/arith_until.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, c;
+  printf("Enter radius (in mm):\n");
+  scanf("%f", &r);
+
+  while(r != 0){
+     a = PI * r * r;
+     c = 2 * PI * r;
+     printf("Circle's area is %3.2f (sq in).\n", a);
+     printf("Circle's circumference is %3.2f (in).\n", c);
+     printf("Enter radius (in mm):\n");
+     scanf("%f", &r);
+  }
+}
