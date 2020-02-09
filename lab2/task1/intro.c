0fe4f468ec0dcbf4c7adcf75ab1cbb4b1ff1675b
Czarina Manipon <cm80117n@pace.edu>
2020-02-08 19:49:50 -0500

    adding skeleton for lab2, complete tasks 1 and 2

---
+++ lab2/task1/intro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a;
+
+  printf("Enter radius (in inches):\n");
+  scanf("%f", &r);
+
+  a = PI * r * r;
+
+  printf("Circle's area is %3.2f (sq in).\n", a);
+}
