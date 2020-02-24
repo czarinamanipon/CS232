a822eae1a10ea7c73795cd742b036a02461ecf15
Czarina Manipon <cm80117n@pace.edu>
2020-02-23 23:58:24 -0500

    Completed task 4 of lab 3

--- lab3/task4/rotate.c
+++ lab3/task4/rotate.c
@@ -3,6 +3,16 @@
 
 void rotate_4(int *p_a, int*p_b, int*p_c, int* p_d) {
     //TO-DO: please implement left rotate 4 integers by one position
+    int a, b, c, d;
+    a = *p_a << 1;
+    *p_a = a;
+    b = *p_b << 1;
+   * p_b = b;
+    c = *p_c << 1;
+    *p_c = c;
+    d = *p_d << 1;
+    *p_d = d;
+    
 }
 
 int main(int argc, char ** argv) {
@@ -13,7 +23,11 @@ int main(int argc, char ** argv) {
     int b = atoi(argv[2]);
     int c = atoi(argv[3]);
     int d = atoi(argv[4]);
-    rotate_4(&a, &b, &c, &d);
-    printf("%d %d %d %d\n", a, b, c, d);
+    if((argc - 1 > 4) || (argc - 1 < 4)){
+        printf("Error: rotate needs four numbers");
+    } else{
+        rotate_4(&a, &b, &c, &d);
+        printf("%d %d %d %d\n", a, b, c, d); 
+    }
     return 0;
 }
