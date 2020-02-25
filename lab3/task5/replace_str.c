2ad4921cc2a1ea70dda806782ef477d94489bc75
Czarina Manipon <cm80117n@pace.edu>
2020-02-24 23:49:24 -0500

    updated task 5 of lab 3

--- lab3/task5/replace_str.c
+++ lab3/task5/replace_str.c
@@ -2,6 +2,39 @@
  * * Does nothing if there is not. */
 #include <stdio.h>
 #include <string.h>
+void CS232(char* src) {
+     char *srcptr, *replaceptr;
+     int srcLength = sizeof(src);
+     char replacement[15] = "232 is awesome!";
+     src = strcpy;
+     replaceptr = &replacement;
+     if (srcLength <= 15) {
+         for (int i = 0; i < 15; i++){
+             printf("srcptr = %c and replaceptr = %c\n", *srcptr, *replaceptr);
+              *src++ = *replaceptr++;
+              //strcpy(src, replacement);
+         }
+     }
+}
+
+char * replace_string() {
+    char *strptr;
+    char str[15];
+    strptr = str;
+    CS232(strptr);
+    return strptr;
+
+}
+
+int main(int argc, char ** argv) {
+    char *ret = (int *)
+ replace_string();
+    printf("replaced string = %s\n", *ret);
+    return 0;
+}/* Overwrites an inputted string with 232 is awesome! if there is room.
+ * * Does nothing if there is not. */
+#include <stdio.h>
+#include <string.h>
 void CS232(char* src, int srcLength) {
      char *srcptr, *replaceptr;
      int srcLength = sizeof(src);
