7f7c82e5ca39b54901726ef61899a3652af79441
Czarina Manipon <cm80117n@pace.edu>
2020-03-09 19:38:17 -0400

    Completed task 1 of lab 5

--- lab5/task1/construct_3_structs.c
+++ lab5/task1/construct_3_structs.c
@@ -4,20 +4,48 @@
 typedef struct snode node_t;
 
 node_t * setup() {
-    node_t * head ;
+    node_t * head;
+    node_t *node1, *node2, *node3;
     //TODO:head declared for you
     //Allocate three more pointers
     //head for the first Node, and temporary pointers node1, node2 and node3
     //Allocate three node pointees and store references to them in the three pointers
     //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
-    //Dereference each pointer to access the .next field in its pointee, 
-    //and use pointer assignment to set the .next field to point to the appropriate Node. 
+    //Dereference each pointer to access the .next field in its pointee,
+    //and use pointer assignment to set the .next field to point to the appropriate Node.
     
+    node1 = malloc(sizeof(node_t));
+    node2 = malloc(sizeof(node_t));
+    node3 = malloc(sizeof(node_t));
+    
+    node1->length = 6;
+    node2->length = 6;
+    node3->length = 5;
+
+    strcpy(node1->str, "hello");
+    strcpy(node2->str, "there");
+    strcpy(node3->str, "prof");
+
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = NULL;
+    
+    head = node1;
    return head;
 }
 
-void teardown() {
+void teardown(node_t * head) {
     //TODO: free all dynamic memory you requested.
+    //Please complete the prototype of teardown.
+    //You are not allowed to use globals
+   int i;
+   node_t *tempNext;
+   for(i = 0; i < 3; i++){
+      tempNext = head->next;
+      free(head);
+      head = tempNext;
+   }
+   free(head);  
 }
 
 //You can ignore the following code for testing
@@ -25,7 +53,7 @@ void dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * head = setup();
     dump_all(head);
-    teardown();
+    teardown(head);
     return 0;
 }
 
@@ -34,7 +62,7 @@ void dump_all(node_t * head) {
     node_t * cur = head;
     while(cur != NULL) {
         printf("%s ", cur->str);
-	cur = cur->next;
+        cur = cur->next;
     }
     printf("\ndone\n ");
 }
