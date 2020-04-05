39d748b4c38ef70276c5810196215a02b9117b17
Czarina Manipon <cm80117n@pace.edu>
2020-04-05 19:05:38 -0400

    Completed task 5 of lab 7

--- lab7/task5/main.c
+++ lab7/task5/main.c
@@ -1,13 +1,20 @@
 char password[8] = "secret";
 int main() {
     char input[8];
+    int length;
     printf("please enter your password\n");
     scanf("%s", input);
 
-    if(strcmp(input, password)==0) {
+   for(length = 0; length <= 7; length++) {
+        if((input[length] >= 65) && (input[length] <= 90)){
+            input[length] = input[length] + 32;
+        }
+        
+    }
+    if(strcmp(input, password) == 0){
         return 0;
-    } else {
+    }else{
         return -1;
     }
-
+    
 }
