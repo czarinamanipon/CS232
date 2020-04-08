389b41ba36f2e2a077217ada90c69a91a0cb6a1b
Czarina Manipon <cm80117n@pace.edu>
2020-04-08 17:56:01 -0400

    Completed task 1 of lab 7

--- lab7/task1/msb.c
+++ lab7/task1/msb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int msb(int x);
 void print_binary(int n);
@@ -30,19 +31,37 @@ int msb(int x) {
 	//                        |
 	//                        ep
 	// in which * is a wild card which could be 0 or 1                   
-	int w, ep;
+	int w, ep, hw, mask, i, j, n;
 	w=32; // Number of bits that might contain most significant 1
 	ep=0; // Rightmost bit that might contain most significant 1
+        mask = 0;
 	while(w>1) { //Narrow down to a single bit
 		//TODO: Look at half the range of bits
-		//TODO: create a mask. 
+                hw = (w / 2);
+		//TODO: create a mask.
+                mask = 1;
+                for(i = w - 1; i > hw; i--){
+                   n = 1;
+                   for(j = i; j > hw; j--){
+                      n = n * 2;
+                   }
+                   mask = mask + n;
+                }
+                mask = (mask << (hw + ep));
 		// This mask is all one-bits in the left half of the range
 		printf("M= "); print_binary(mask); printf(" hw=%d ep=%d\n",hw,ep);
 		//TODO: use mask to figure out if the left half of the range has at least one bit   
 		//If the left half of the range has a one bit, focus on only the left half
 		//else focus on only the right half
+                if(((x >> hw) & (mask >> hw)) != 0){ //left
+                   if(w > 2){
+                     ep = hw + ep;
+                   }
+                }
+                   
 		//TODO: You have ruled out either the left half of the range or the right half of the range
 		//set up ep and w accordingly
+                   w = w / 2;
 	}
 	return ep;
 }
